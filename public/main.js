9bccdf20abfa071244a9cb40e796b8607c9f03a7
Sabrina Mok <sabrinamok@MacBook-Pro.local>
2022-04-01 09:34:48 +0800

    new edits

--- public/main.js
+++ public/main.js
@@ -1,7 +1,7 @@
 const path = require("path")
 const fs = require("fs")
 
-const dirPath = path.join(__dirname, "../collection")
+const dirPath = path.join(__dirname, "../nft")
 const dirPathPages = path.join(__dirname, "../src/pages/content")
 let postlist = []
 let pagelist = []
