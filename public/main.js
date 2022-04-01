ab49f7cbe7d68688530e6298c107a0516b75ae25
Sabrina Mok <sabrinamok@MacBook-Pro.local>
2022-04-01 11:29:09 +0800

    edit

--- public/main.js
+++ public/main.js
@@ -103,74 +103,6 @@ const getPosts = () => {
     return 
 }
 
-const getCollections = () => {
-    fs.readdir(dirPathCollections, (err, files) => {
-        if (err) {
-            return console.log("Failed to list contents of directory: " + err)
-        }
-        let ilist = []
-        files.forEach((file, i) => {
-            let obj = {}
-            let post
-            fs.readFile(`${dirPathCollections}/${file}`, "utf8", (err, contents) => {
-                const getMetadataIndices = (acc, elem, i) => {
-                    if (/^---/.test(elem)) {
-                        acc.push(i)
-                    }
-                    return acc
-                }
-                const parseMetadata = ({lines, metadataIndices}) => {
-                    if (metadataIndices.length > 0) {
-                        let metadata = lines.slice(metadataIndices[0] + 1, metadataIndices[1])
-                        metadata.forEach(line => {
-                            obj[line.split(": ")[0]] = line.split(": ")[1]
-                        })
-                        return obj
-                    }
-                }
-                const parseContent = ({lines, metadataIndices}) => {
-                    if (metadataIndices.length > 0) {
-                        lines = lines.slice(metadataIndices[1] + 1, lines.length)
-                    }
-                    return lines.join("\n")
-                }
-                const lines = contents.split("\n")
-                const metadataIndices = lines.reduce(getMetadataIndices, [])
-                const metadata = parseMetadata({lines, metadataIndices})
-                const content = parseContent({lines, metadataIndices})
-                const parsedDate = metadata.date ? formatDate(metadata.date) : new Date()
-                const publishedDate = `${parsedDate["monthName"]} ${parsedDate["day"]}, ${parsedDate["year"]}`
-                const datestring = `${parsedDate["year"]}-${parsedDate["month"]}-${parsedDate["day"]}T${parsedDate["time"]}:00`
-                const date = new Date(datestring)
-                const timestamp = date.getTime() / 1000
-                post = {
-                    id: timestamp,
-                    title: metadata.title ? metadata.title : "No title given",
-                    date: publishedDate ? publishedDate : "No date given",
-                    time: parsedDate["time"],
-                    thumbnail: metadata.thumbnail,
-                    content: content ? content : "No content given",
-                    price: metadata.price ? metadata.price : "No price given",
-                    collection: metadata.collection ? metadata.collection : "No collection given",
-                    ongoing: metadata.ongoing ? metadata.ongoing : "",
-                    launchpad: metadata.launchpad ? metadata.launchpad : "",
-                    hot: metadata.hot ? metadata.hot : ""
-                }
-                collectionlist.push(post)
-                ilist.push(i)
-                if (ilist.length === files.length) {
-                    const sortedList = postlist.sort ((a, b) => {
-                        return a.id < b.id ? 1 : -1
-                    })
-                    let data = JSON.stringify(sortedList)
-                    fs.writeFileSync("src/collections.json", data)
-                }
-            })
-        })
-    })
-    return 
-}
-
 const getPages = () => {
     fs.readdir(dirPathPages, (err, files) => {
         if (err) {
@@ -192,5 +124,4 @@ const getPages = () => {
 }
 
 getPosts()
-getCollections()
 getPages()
