da9894ca4fbd33c65328518868b839ced0655100
Sabrina Mok <sabrinamok@MacBook-Pro.local>
2022-04-04 12:35:30 +0800

    ordering

--- src/components/CollectionPage3.js
+++ src/components/CollectionPage3.js
@@ -135,7 +135,7 @@ function CollectionPage3(props) {
         />
         <Grid container={true} spacing={4}>
         {postlist.length && 
-              postlist.sort((a,b) => (b.order != null) - (a.order != null) || a.order - b.order).map((post, i) => {
+              postlist((a, b) => (a.order != null ? a.order : 0) - (b.order != null ? b.order : 0)).map((post, i) => {
                 return (
             <Grid item={true} xs={12} sm={6} md={3} lg={3} key={i}>
               <Card className={classes.card}>
